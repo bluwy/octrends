2bbe93082db3b8ad9649dc7cc20c4d8dff1cd2bb
bluwy <bjornlu.dev@gmail.com>
2026-03-04 14:21:31 +0800

    Show top expense sources

--- src/components/sections/ExpenseSection.vue
+++ src/components/sections/ExpenseSection.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import MonthlyExpenseChart from '../charts/MonthlyExpenseChart.vue'
 import type { CollectiveData } from '../../utils/types'
+import TopExpenseSourcesTable from '../tables/TopExpenseSourcesTable.vue'
 
 const props = defineProps<{
   data: CollectiveData[]
@@ -10,7 +11,7 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div>
+  <div class="my-8">
     <h3 class="text-xl font-400 m-0">Expenses</h3>
     <div class="flex justify-between gap-8">
       <div class="flex-1">
@@ -21,7 +22,11 @@ const props = defineProps<{
         />
       </div>
       <div class="flex-1">
-        <!-- Reserved for future: Last balances or summary -->
+        <TopExpenseSourcesTable
+          :data="props.data"
+          :earliestDate="props.earliestDate"
+          :latestDate="props.latestDate"
+        />
       </div>
     </div>
   </div>
